ffc6a49f574d5ce108f1d290d649bf1f4d2bef08
qq1311031761 <1311031761@qq.com>
2019-07-10 16:26:44 +0800

    first commit

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>MemberContainer</h3>
+  </div>
+</template>
+
+<script>
+import { Toast } from 'mint-ui';
+export default {
+  created(){
+    Toast({
+      message: '对不起,服务器错误',
+      iconClass: 'icon icon-success'
+  });
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
